ce3f920e96e47bb8ba53f0ab734ae910999e6d55
Collins Ndung'u <collinsndungu953@gmail.com>
2024-12-04 09:59:50 +0300

    add admin page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePage from './Pages/homepage';
+import AdminPage from 'src/Pages/AdminPage'
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <AdminPage/>
     </div>
   );
 }
